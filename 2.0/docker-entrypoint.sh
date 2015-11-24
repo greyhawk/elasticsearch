b3b4db60a69846c15517b3c31b0194a87e96dcf6
greyhawk <wu.tongqing@gmail.com>
2015-11-24 17:37:19 +0800

    modify 1000

--- 2.0/docker-entrypoint.sh
+++ 2.0/docker-entrypoint.sh
@@ -10,8 +10,8 @@ fi
 # Drop root privileges if we are running elasticsearch
 if [ "$1" = 'elasticsearch' ]; then
 	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
-	chown -R 1000:elasticsearch /usr/share/elasticsearch/data
-	exec gosu 1000 "$@"
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+	exec gosu elasticsearch "$@"
 fi
 
 # As argument is not related to elasticsearch,
